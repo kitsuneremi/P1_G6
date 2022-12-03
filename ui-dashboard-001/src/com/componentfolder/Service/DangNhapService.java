6823dcb8fe7c1fd93670a760c3b337fa6fb4c6e4
Nguyễn Mạnh Hà <109425188+CrowHa@users.noreply.github.com>
2022-12-03 12:19:37 +0700

    Ha 3/12/2022

---
+++ ui-dashboard-001/src/com/componentfolder/Service/DangNhapService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.componentfolder.Service;
+
+import com.componentfolder.Model.DangNhap;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HA NGUYEN
+ */
+public interface DangNhapService {
+    public String get(String tk,String mk);
+}
