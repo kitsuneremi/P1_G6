fde2c4ecdaee723b1f229b20ed6c82f365d09821
Nguyễn Mạnh Hà <109425188+CrowHa@users.noreply.github.com>
2022-12-03 15:16:28 +0700

    Ha

--- ui-dashboard-001/src/com/componentfolder/Repository/KhachHangRepo.java
+++ ui-dashboard-001/src/com/componentfolder/Repository/KhachHangRepo.java
@@ -7,7 +7,7 @@ package com.componentfolder.Repository;
 
 
 import Utilities.DBConnections_Ha;
-import ViewModel.KhachHangViewModel;
+import com.componentfolder.ViewModel.KhachHangViewModel;
 import com.componentfolder.Model.KhachHangModel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -40,14 +40,15 @@ public class KhachHangRepo {
         
     }
     public Boolean add(KhachHangModel kh){
-        String sql = "insert into khachhang(id,ten,sdt,gioitinh,cccd,makh) values(NEWID(),?,?,?,?,?)";
+        String sql = "insert into khachhang(makh,ten,sdt,gioitinh,cccd,gmail) values(?,?,?,?,?,?)";
          try (Connection con = connections_Ha.getConnection();){
            PreparedStatement ps2 = con.prepareStatement(sql);
-           ps2.setObject(1, kh.getTen());
-           ps2.setObject(2, kh.getSdt());
-           ps2.setObject(3, kh.getGioitinh());
-           ps2.setObject(4, kh.getCccd());
-           ps2.setObject(5, kh.getMakh());
+           ps2.setObject(2, kh.getTen());
+           ps2.setObject(3, kh.getSdt());
+           ps2.setObject(4, kh.getGioitinh());
+           ps2.setObject(5, kh.getCccd());
+           ps2.setObject(1, kh.getMakh());
+           ps2.setObject(6, kh.getEmail());
            ps2.executeUpdate();
            return true;
        } catch (Exception e) {
@@ -57,7 +58,7 @@ public class KhachHangRepo {
             
     }
     public Boolean update(KhachHangModel kh){
-        String sql = "Update khachhang set ten = ?,sdt = ? ,gioitinh = ?,cccd =?,Where MaKh = ?";
+        String sql = "Update khachhang set ten = ?,sdt = ? ,gioitinh = ?,cccd =?, email = ? Where MaKh = ?";
          try (Connection con = connections_Ha.getConnection();){
            PreparedStatement ps2 = con.prepareStatement(sql);
            ps2.setObject(1, kh.getTen());
@@ -65,6 +66,7 @@ public class KhachHangRepo {
            ps2.setObject(3, kh.getGioitinh());
            ps2.setObject(4, kh.getCccd());
            ps2.setObject(5, kh.getMakh());
+           ps2.setObject(6, kh.getMakh());
            ps2.executeUpdate();
            return true;
        } catch (Exception e) {
@@ -75,7 +77,7 @@ public class KhachHangRepo {
     }
     public int getcountDB(){
         int mkh = 0;
-        String Sql = "Select  COUNT(id) from KhachHang";
+        String Sql = "Select  COUNT(makh) from KhachHang";
         try(Connection con = connections_Ha.getConnection();
                 PreparedStatement ps = con.prepareStatement(Sql)) {
             ResultSet rs = ps.executeQuery();
@@ -87,6 +89,35 @@ public class KhachHangRepo {
             e.printStackTrace();
         }
         return mkh;
+        
+    }
+    public ArrayList<KhachHangViewModel> search(String ten){
+        String sql = "Select makh, Ten, sdt,gioitinh , cccd,email from khachhang where makh =? or ten = ? or sdt = ? or cccd = ? or email = ?";
+        ArrayList<KhachHangViewModel> listKH = new ArrayList<>();
+        try (Connection con = connections_Ha.getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, ten);
+            ps.setString(2, ten);
+            ps.setString(3, ten);
+            ps.setString(4, ten);
+            ps.setString(5, ten);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                KhachHangViewModel g = new KhachHangViewModel();
+                g.setMakh(rs.getString(1));
+                g.setTen(rs.getString(2));
+                g.setSdt(rs.getString(3));
+                g.setGioitinh(rs.getInt(4));
+                g.setCccd(rs.getString(5));
+                g.setEmail(rs.getString(6));
+                listKH.add(g);
+
+            }
+            return listKH;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
         
     }
 }
