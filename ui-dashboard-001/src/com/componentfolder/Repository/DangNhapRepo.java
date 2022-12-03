e3f626c227a020863a010eadc004f59ee0c5f21a
Nguyễn Mạnh Hà <109425188+CrowHa@users.noreply.github.com>
2022-12-03 12:18:56 +0700

    Ha 3/12/2022

---
+++ ui-dashboard-001/src/com/componentfolder/Repository/DangNhapRepo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.componentfolder.Repository;
+
+import Utilities.DBConnections_Ha;
+import ViewModel.KhachHangViewModel;
+import com.componentfolder.Model.DangNhap;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HA NGUYEN
+ */
+public class DangNhapRepo {
+
+    private DBConnections_Ha connections;
+
+    public String get(String tk, String mk) {
+        DangNhap g = new DangNhap();
+        String sql = "Select chucvu from dangnhap where taikhoan =? or matkhau = ?  ";
+        try (Connection con = connections.getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, tk);
+            ps.setString(2, mk);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                
+                g.setChucvu(rs.getString(1));
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return g.getChucvu();
+
+    }
+}
