67d92fcfd22c70dbd135fdba9a5cc6edd08c3c19
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2022-12-08 14:29:22 +0700

    8/12

--- ui-dashboard-001/src/com/componentfolder/Repository/HoaDonTraXeRepo.java
+++ ui-dashboard-001/src/com/componentfolder/Repository/HoaDonTraXeRepo.java
@@ -1,11 +1,13 @@
 package com.componentfolder.Repository;
 
-import Utilities.DBConnections1;
+import Utilities.DBConnections;
 import com.componentfolder.Model.ChiTietHopDongModel;
+import com.componentfolder.Model.ChiTietViPhamModel;
 import com.componentfolder.ViewModel.HoaDonTraXeViewModel;
 import com.componentfolder.ViewModel.KhachHangViewModel;
 import com.componentfolder.Model.HoaDonTraXeModel;
 import com.componentfolder.Model.HopDongModel;
+import com.componentfolder.Model.ViPhamModel;
 import com.componentfolder.ViewModel.HopDongViewModel;
 import java.util.ArrayList;
 import java.sql.*;
@@ -19,20 +21,20 @@ public class HoaDonTraXeRepo {
     public ArrayList<HoaDonTraXeViewModel> getlistdata() {
         try {
             ArrayList<HoaDonTraXeViewModel> list = new ArrayList<>();
-            String sql = "select hoadontraxe.mahoadon,hopdong.mahopdong,ngaytra,hoadontraxe.mavipham,phiphatsinh,thanhtien,hopdong.makh from hoadontraxe join hopdong on hoadontraxe.mahopdong = hopdong.mahopdong";
-            Connection conn = DBConnections1.getConnection();
+            String sql = "select hoadontraxe.mahoadon,hopdong.mahopdong,ngaytra,chitietvipham.mavipham,phiphatsinh,thanhtien,hopdong.makh from hoadontraxe join hopdong on hoadontraxe.mahopdong = hopdong.mahopdong join chitietvipham on chitietvipham.mahoadon = hoadontraxe.mahoadon";
+            Connection conn = DBConnections.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                HoaDonTraXeViewModel hdhtvm = new HoaDonTraXeViewModel();
-                hdhtvm.setId(rs.getString(1));
-                hdhtvm.setIdhd(rs.getString(2));
-                hdhtvm.setNgaytra(rs.getDate(3));
-                hdhtvm.setVipham(rs.getInt(4));
-                hdhtvm.setPhiphatsinh(rs.getDouble(5));
-                hdhtvm.setTongtien(rs.getDouble(6));
-                hdhtvm.setIdkh(rs.getString(7));
-                list.add(hdhtvm);
+                HoaDonTraXeViewModel hdtxvm = new HoaDonTraXeViewModel();
+                hdtxvm.setId(rs.getString(1));
+                hdtxvm.setIdhd(rs.getString(2));
+                hdtxvm.setNgaytra(rs.getDate(3));
+                hdtxvm.setVpm(new ViPhamModel(rs.getInt(4), ""));
+                hdtxvm.setPhiphatsinh(rs.getDouble(5));
+                hdtxvm.setTongtien(rs.getDouble(6));
+                hdtxvm.setIdkh(rs.getString(7));
+                list.add(hdtxvm);
             }
             return list;
         } catch (SQLException ex) {
@@ -45,7 +47,7 @@ public class HoaDonTraXeRepo {
         try {
             ArrayList<HoaDonTraXeViewModel> list = new ArrayList<>();
             String sql = "select hoadontraxe.mahoadon from hoadontraxe where ngayTra = '2099-12-30'";
-            Connection conn = DBConnections1.getConnection();
+            Connection conn = DBConnections.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
@@ -61,7 +63,7 @@ public class HoaDonTraXeRepo {
     }
 
     public KhachHangViewModel getkh(String idhd) {
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             String sql = "select khachhang.makh,ten,email from khachhang join hopdong on khachhang.makh = hopdong.makh where hopdong.mahopdong = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, idhd);
@@ -80,7 +82,7 @@ public class HoaDonTraXeRepo {
     }
 
     public String getnv(String idnv) {
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             String sql = "select ten from nhanvien where idnv = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, idnv);
@@ -95,7 +97,7 @@ public class HoaDonTraXeRepo {
     }
 
     public ArrayList<HopDongViewModel> getlisthd() {
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             ArrayList<HopDongViewModel> list = new ArrayList<>();
             String sql = "select hopdong.mahopdong,idnv,hopdong.makh,ngaytao,ngayhethan,tinhtrang,ten,email from hopdong join khachhang on hopdong.makh = khachhang.makh order by hopdong.mahopdong desc";
             PreparedStatement ps = conn.prepareStatement(sql);
@@ -117,16 +119,16 @@ public class HoaDonTraXeRepo {
         }
     }
 
-    public boolean insert(HoaDonTraXeModel hdhtm) {
-        try ( Connection conn = DBConnections1.getConnection()) {
-            String sql = "insert into hoadontraxe(mahoadon,ngaytra,mavipham,phiphatsinh,thanhtien,mota) values(?,?,?,?,?,?)";
+    public boolean insert(HoaDonTraXeModel hdtxm) {
+        try ( Connection conn = DBConnections.getConnection()) {
+            String sql = "insert into hoadontraxe(mahoadon,ngaytra,phiphatsinh,thanhtien,mota) values(?,?,?,?,?)";
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, hdhtm.getId());
-            ps.setDate(2, new java.sql.Date(hdhtm.getNgaytra().getTime()));
-            ps.setInt(3, hdhtm.getVipham().getMa());
-            ps.setDouble(4, hdhtm.getPhiphatsinh());
-            ps.setDouble(5, hdhtm.getTongtien());
-            ps.setString(6, hdhtm.getMota());
+            ps.setString(1, hdtxm.getId());
+            ps.setDate(2, new java.sql.Date(hdtxm.getNgaytra().getTime()));
+            ps.setDouble(3, hdtxm.getPhiphatsinh());
+            ps.setDouble(4, hdtxm.getTongtien());
+            ps.setString(5, hdtxm.getMota());
+            ps.executeUpdate();
             return ps.executeUpdate() > 0;
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -134,17 +136,29 @@ public class HoaDonTraXeRepo {
         }
     }
 
-    public boolean update(HoaDonTraXeModel hdhtm) {
-        try ( Connection conn = DBConnections1.getConnection()) {
-            String sql = "update hoadontraxe set mahopdong = ?,ngaytra = ?, mavipham = ?, phiphatsinh = ?, thanhtien = ?, mota = ? where mahoadon = ?";
+    public boolean update(HoaDonTraXeModel hdtxm) {
+        try ( Connection conn = DBConnections.getConnection()) {
+            String sql = "update hoadontraxe set mahopdong = ?,ngaytra = ?, phiphatsinh = ?, thanhtien = ?, mota = ? where mahoadon = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, hdhtm.getHdm().getMahd());
-            ps.setDate(2, new java.sql.Date(hdhtm.getNgaytra().getTime()));
-            ps.setInt(3, hdhtm.getVipham().getMa());
-            ps.setDouble(4, hdhtm.getPhiphatsinh());
-            ps.setDouble(5, hdhtm.getTongtien());
-            ps.setString(6, hdhtm.getMota());
-            ps.setString(7, hdhtm.getId());
+            ps.setString(1, hdtxm.getHdm().getMahd());
+            ps.setDate(2, new java.sql.Date(hdtxm.getNgaytra().getTime()));
+            ps.setDouble(3, hdtxm.getPhiphatsinh());
+            ps.setDouble(4, hdtxm.getTongtien());
+            ps.setString(5, hdtxm.getMota());
+            ps.setString(6, hdtxm.getId());
+            return ps.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean updatevipham(ChiTietViPhamModel ctvpm) {
+        try ( Connection conn = DBConnections.getConnection()) {
+            String sql = "insert into chitietvipham(mahoadon,mavipham) values(?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, ctvpm.getMahoadon());
+            ps.setInt(2, ctvpm.getVpm().getMa());
             return ps.executeUpdate() > 0;
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -153,7 +167,7 @@ public class HoaDonTraXeRepo {
     }
 
     public ArrayList<ChiTietHopDongModel> getdsbienso(String mahd) {
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             ArrayList<ChiTietHopDongModel> list = new ArrayList<>();
             String sql = "select mahopdong,bienso from chitiethopdong where mahopdong = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
@@ -172,7 +186,7 @@ public class HoaDonTraXeRepo {
     }
 
     public boolean updatexe(String bienso) {
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             String sql = "update chitietxe set tinhtrangxe = ? where bien so = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, bienso);
@@ -184,7 +198,7 @@ public class HoaDonTraXeRepo {
     }
 
     public boolean delete(HoaDonTraXeModel hdhtm) {
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             String sql = "delete from hoadontraxe where id = convert(uniqueidentifier,?)";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, hdhtm.getId());
@@ -199,7 +213,7 @@ public class HoaDonTraXeRepo {
         if (idhd == null || idhd.equalsIgnoreCase("")) {
             return 0;
         }
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             double tienthuexe = 0;
             String sql = "select chitietxe.giathuetrenngay from hopdong join chitiethopdong on hopdong.mahopdong = chiTietHopDong.mahopdong join chiTietXe on chiTietHopDong.bienSo = chiTietXe.bienSo where hopDong.mahopdong = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
@@ -217,7 +231,7 @@ public class HoaDonTraXeRepo {
 
     public String getemail(String makh) {
         String sql = "select email from khachhang join hopdong on khachhang.makh = hopdong.makh where hopdong.mahopdong = ?";
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, makh);
             ResultSet rs = ps.executeQuery();
@@ -231,14 +245,14 @@ public class HoaDonTraXeRepo {
         }
     }
 
-    public ArrayList<String> vipham() {
-        String sql = "select ten from vipham";
-        ArrayList<String> list = new ArrayList<>();
-        try ( Connection conn = DBConnections1.getConnection()) {
+    public ArrayList<ViPhamModel> vipham() {
+        String sql = "select mavipham,ten from vipham";
+        ArrayList<ViPhamModel> list = new ArrayList<>();
+        try ( Connection conn = DBConnections.getConnection()) {
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                list.add(rs.getString(1));
+                list.add(new ViPhamModel(rs.getInt(1), rs.getString(2)));
             }
             return list;
         } catch (Exception ex) {
@@ -247,11 +261,11 @@ public class HoaDonTraXeRepo {
         }
     }
 
-    public String mota(String idhd) {
+    public String mota(String mahd) {
         String sql = "select mota from hoadontraxe where mahoadon = ?";
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, idhd);
+            ps.setString(1, mahd);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 return rs.getString(1);
@@ -263,12 +277,12 @@ public class HoaDonTraXeRepo {
         }
     }
 
-    public HopDongViewModel gethdx(String idhd) {
+    public HopDongViewModel gethdx(String mahd) {
 
         String sql = "select ngaytao,ngayhethan from hopdong where mahopdong = ?";
-        try ( Connection conn = DBConnections1.getConnection()) {
+        try ( Connection conn = DBConnections.getConnection()) {
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, idhd);
+            ps.setString(1, mahd);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 HopDongViewModel hdvm = new HopDongViewModel();
@@ -280,6 +294,23 @@ public class HoaDonTraXeRepo {
         } catch (Exception ex) {
             ex.printStackTrace();
             return null;
+        }
+
+    }
+
+    public int getsovipham(String mahd) {
+        String sql = "select count(mavipham) from chitietvipham where mahoadon = ?";
+        try ( Connection conn = DBConnections.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, mahd);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+            return 0;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return 0;
         }
 
     }
