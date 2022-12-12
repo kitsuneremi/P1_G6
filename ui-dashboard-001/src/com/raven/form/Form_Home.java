ed026510be757bf453b9dfae88651d71bad25255
Nguyễn Mạnh Hà <109425188+CrowHa@users.noreply.github.com>
2022-12-13 00:42:58 +0700

    UpLoad Form Home 99%

--- ui-dashboard-001/src/com/raven/form/Form_Home.java
+++ ui-dashboard-001/src/com/raven/form/Form_Home.java
@@ -1,6 +1,7 @@
 package com.raven.form;
 
 //import com.raven.model.Model_Card;
+import com.componentfolder.Repository.AnalystRepo;
 import com.componentfolder.Service.CTXeSevices;
 import com.componentfolder.Service.Impl.CTXeServiceImpl;
 import com.componentfolder.ViewModel.Top10ViewModel;
@@ -10,11 +11,19 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
+import java.awt.Color;
+import com.raven.javaswingdev.chart.ModelPieChart;
+import com.raven.javaswingdev.chart.PieChart;
+import com.raven.javaswingdev.chart.PieChartRepo;
 
 public class Form_Home extends javax.swing.JPanel {
     private DefaultTableModel dtm = new DefaultTableModel();
     private List<Top10ViewModel> listTop10 = new ArrayList<>();
     private CTXeSevices cTXeSevices = new CTXeServiceImpl();
+    private AnalystRepo alr = new AnalystRepo();
+    private PieChartRepo repo = new PieChartRepo();
+    private ModelPieChart md = new ModelPieChart();
+    int aa = 10;
 
     public Form_Home() {
         initComponents();
@@ -23,55 +32,48 @@ public class Form_Home extends javax.swing.JPanel {
         init();
         listTop10 = cTXeSevices.getTop10ThueXe();
         showDataTop10(listTop10);
+        loadChart();
 
     }
+    public void loadChart(){
+        pieChart1.setBackground(Color.white);
+        pieChart1.setSize(500, 500);
+        pieChart1.setChartType(PieChart.PeiChartType.DONUT_CHART);
+        pieChart1.addData(new ModelPieChart("Xe có thể cho thuê", repo.XenTrong(), new Color(23, 126, 238)));
+        pieChart1.addData(new ModelPieChart("Xe đang cho thuê", repo.XeDangChoThue(), new Color(221, 65, 65)));
+        pieChart1.addData(new ModelPieChart("Xe đang hỏng hóc", repo.XeDangSua(), new Color(47, 157, 64)));
+        
+    }
     private void showDataTop10(List<Top10ViewModel> list){
-        dtm = (DefaultTableModel) tblTop10.getModel();
-        dtm.setRowCount(0);
-        for(Top10ViewModel top : list){
-            dtm.addRow(top.toRowDataTop10());
-        }
+//        dtm = (DefaultTableModel) tblTop10.getModel();
+//        dtm.setRowCount(0);
+//        for(Top10ViewModel top : list){
+//            dtm.addRow(top.toRowDataTop10());
+//        }
     }
 
     private void initCardData() {
         ModelCard c1 = new ModelCard();
-        c1.setTitle("Hợp đồng");
-        c1.setValues(100);
+        c1.setTitle("Hợp Đồng Đã Ký");
+        c1.setValues(alr.TongHopDong());
         card1.setData(c1);
-        
         ModelCard c2 = new ModelCard();
-        c2.setTitle("Khách hàng");
-        c2.setValues(110);
-        card2.setData(c2);
-        
+        c2.setTitle("Doanh Thu(Triệu VNĐ)");
+        c2.setValues(alr.HoaDonTong());
+        card3.setData(c2);
         ModelCard c3 = new ModelCard();
         c3.setTitle("Xe");
-        c3.setValues(100);
-        card3.setData(c3);
-        
-        ModelCard c4 = new ModelCard();
-        c4.setTitle("Xe đã thuê");
-        c4.setValues(100);
-        card4.setData(c4);
-        
-        ModelCard c5 = new ModelCard();
-        c5.setTitle("Doanh thu");
-        c5.setValues(100);
-        card5.setData(c5);
+        c3.setValues(alr.TongXe());
+        card5.setData(c3);
 
     }
 
     private void init() {
         chart.addLegend("Số Lượng Hợp Đồng", new Color(12, 84, 175), new Color(0, 108, 247));
-        chart.addLegend("Số Lượng Xe Thuê", new Color(54, 4, 143), new Color(104, 49, 200));
-        chart.addLegend("Số Lượng Khách Hàng", new Color(5, 125, 0), new Color(95, 209, 69));
-        chart.addLegend("Số Tiền", new Color(186, 37, 37), new Color(241, 100, 120));
-        chart.addData(new ModelChart("January", new double[]{500, 200, 80, 89}));
-        chart.addData(new ModelChart("February", new double[]{1000, 750, 90, 150}));
-        chart.addData(new ModelChart("March", new double[]{200, 350, 460, 900}));
-        chart.addData(new ModelChart("April", new double[]{480, 150, 750, 700}));
-        chart.addData(new ModelChart("May", new double[]{350, 540, 300, 150}));
-        chart.addData(new ModelChart("June", new double[]{190, 280, 81, 200}));
+        chart.addLegend("Số Tiền(Triệu VNĐ)", new Color(186, 37, 37), new Color(241, 100, 120));
+        chart.addData(new ModelChart("Tháng 10", new double[]{alr.HopDongThang10(), alr.HoaDonThang10()})); 
+        chart.addData(new ModelChart("Tháng 11", new double[]{alr.HopDongThang11(), alr.HoaDonThang11()}));
+        chart.addData(new ModelChart("Tháng 12", new double[]{alr.HopDongThang12(), alr.HoaDonThang12()}));
         chart.start();
     }
 
@@ -81,30 +83,21 @@ public class Form_Home extends javax.swing.JPanel {
 
         panel = new javax.swing.JLayeredPane();
         card1 = new com.ViewGiaoDienChinh.component.Card();
-        card2 = new com.ViewGiaoDienChinh.component.Card();
         card3 = new com.ViewGiaoDienChinh.component.Card();
-        card4 = new com.ViewGiaoDienChinh.component.Card();
         card5 = new com.ViewGiaoDienChinh.component.Card();
         panelBorder1 = new com.ViewGiaoDienChinh.swing.PanelBorder();
         chart = new com.raven.chart.CurveChart();
-        panelBorder2 = new com.ViewGiaoDienChinh.swing.PanelBorder();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tblTop10 = new javax.swing.JTable();
-        jLabel1 = new javax.swing.JLabel();
+        btnCaNam = new javax.swing.JButton();
+        cbbQuy = new javax.swing.JComboBox<>();
+        pieChart1 = new com.raven.javaswingdev.chart.PieChart();
 
         panel.setLayout(new java.awt.GridLayout(1, 0, 10, 0));
 
         card1.setBackground(new java.awt.Color(117, 29, 169));
         panel.add(card1);
 
-        card2.setBackground(new java.awt.Color(11, 40, 114));
-        panel.add(card2);
-
         card3.setBackground(new java.awt.Color(51, 87, 123));
         panel.add(card3);
-
-        card4.setBackground(new java.awt.Color(121, 12, 12));
-        panel.add(card4);
 
         card5.setBackground(new java.awt.Color(16, 93, 31));
         panel.add(card5);
@@ -128,55 +121,20 @@ public class Form_Home extends javax.swing.JPanel {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        panelBorder2.setBackground(new java.awt.Color(23, 21, 21));
-        panelBorder2.setForeground(new java.awt.Color(51, 51, 51));
-
-        tblTop10.setAutoCreateRowSorter(true);
-        tblTop10.setBackground(new java.awt.Color(51, 51, 51));
-        tblTop10.setFont(new java.awt.Font("Segoe UI", 2, 14)); // NOI18N
-        tblTop10.setForeground(new java.awt.Color(204, 204, 204));
-        tblTop10.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Mã Xe", "Tên Xe", "Số Lượt Thuê", "Mô Tả"
+        btnCaNam.setText("Cả Năm");
+        btnCaNam.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCaNamActionPerformed(evt);
             }
-        ));
-        tblTop10.setSelectionForeground(new java.awt.Color(102, 102, 102));
-        jScrollPane1.setViewportView(tblTop10);
+        });
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 2, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Top 10 Xe Thuê Nhiều Nhất");
-
-        javax.swing.GroupLayout panelBorder2Layout = new javax.swing.GroupLayout(panelBorder2);
-        panelBorder2.setLayout(panelBorder2Layout);
-        panelBorder2Layout.setHorizontalGroup(
-            panelBorder2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelBorder2Layout.createSequentialGroup()
-                .addGap(316, 316, 316)
-                .addComponent(jLabel1)
-                .addContainerGap(341, Short.MAX_VALUE))
-            .addComponent(jScrollPane1)
-        );
-        panelBorder2Layout.setVerticalGroup(
-            panelBorder2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelBorder2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
+        cbbQuy.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Quý 1", "Quý 2", "Quý 3", "Quý 4" }));
+        cbbQuy.setSelectedIndex(3);
+        cbbQuy.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbbQuyActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -185,36 +143,95 @@ public class Form_Home extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(panelBorder2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(panelBorder1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(panel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 875, Short.MAX_VALUE))
-                .addGap(20, 20, 20))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, 875, Short.MAX_VALUE)
+                        .addGap(20, 20, 20))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(132, 132, 132)
+                        .addComponent(btnCaNam)
+                        .addGap(75, 75, 75)
+                        .addComponent(cbbQuy, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(210, 210, 210)
+                        .addComponent(pieChart1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(289, 289, 289)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnCaNam)
+                            .addComponent(cbbQuy, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(3, 3, 3)
+                        .addComponent(pieChart1, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(1, 1, 1)
                 .addComponent(panelBorder1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(panelBorder2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(30, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void cbbQuyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbbQuyActionPerformed
+        // TODO add your handling code here:
+        chart.clear();
+        if(cbbQuy.getSelectedIndex()== 0){
+        chart.addData(new ModelChart("Tháng 1", new double[]{alr.HopDongThang1(), alr.HoaDonThang1()})); 
+        chart.addData(new ModelChart("Tháng 2", new double[]{alr.HopDongThang2(), alr.HoaDonThang2()}));
+        chart.addData(new ModelChart("Tháng 3", new double[]{alr.HopDongThang3(), alr.HoaDonThang3()}));
+        chart.start();
+        }if(cbbQuy.getSelectedIndex()== 1){
+        chart.addData(new ModelChart("Tháng 4", new double[]{alr.HopDongThang4(), alr.HoaDonThang4()})); 
+        chart.addData(new ModelChart("Tháng 5", new double[]{alr.HopDongThang5(), alr.HoaDonThang5()}));
+        chart.addData(new ModelChart("Tháng 6", new double[]{alr.HopDongThang6(), alr.HoaDonThang6()}));
+        chart.start();
+        }if(cbbQuy.getSelectedIndex()== 2){
+        chart.addData(new ModelChart("Tháng 7", new double[]{alr.HopDongThang7(), alr.HoaDonThang7()})); 
+        chart.addData(new ModelChart("Tháng 8", new double[]{alr.HopDongThang8(), alr.HoaDonThang8()}));
+        chart.addData(new ModelChart("Tháng 9", new double[]{alr.HopDongThang9(), alr.HoaDonThang9()}));
+        chart.start();
+        }if(cbbQuy.getSelectedIndex()== 3){
+        chart.addData(new ModelChart("Tháng 10", new double[]{alr.HopDongThang10(), alr.HoaDonThang10()})); 
+        chart.addData(new ModelChart("Tháng 11", new double[]{alr.HopDongThang11(), alr.HoaDonThang11()}));
+        chart.addData(new ModelChart("Tháng 12", new double[]{alr.HopDongThang12(), alr.HoaDonThang12()}));
+        chart.start();
+        }
+    }//GEN-LAST:event_cbbQuyActionPerformed
+
+    private void btnCaNamActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCaNamActionPerformed
+        // TODO add your handling code here:
+        chart.clear();
+        chart.addData(new ModelChart("Tháng 1", new double[]{alr.HopDongThang1(), alr.HoaDonThang1()})); 
+        chart.addData(new ModelChart("Tháng 2", new double[]{alr.HopDongThang2(), alr.HoaDonThang2()}));
+        chart.addData(new ModelChart("Tháng 3", new double[]{alr.HopDongThang3(), alr.HoaDonThang3()}));
+        chart.addData(new ModelChart("Tháng 4", new double[]{alr.HopDongThang4(), alr.HoaDonThang4()})); 
+        chart.addData(new ModelChart("Tháng 5", new double[]{alr.HopDongThang5(), alr.HoaDonThang5()}));
+        chart.addData(new ModelChart("Tháng 6", new double[]{alr.HopDongThang6(), alr.HoaDonThang6()}));
+        chart.addData(new ModelChart("Tháng 7", new double[]{alr.HopDongThang7(), alr.HoaDonThang7()})); 
+        chart.addData(new ModelChart("Tháng 8", new double[]{alr.HopDongThang8(), alr.HoaDonThang8()}));
+        chart.addData(new ModelChart("Tháng 9", new double[]{alr.HopDongThang9(), alr.HoaDonThang9()}));
+        chart.addData(new ModelChart("Tháng 10", new double[]{alr.HopDongThang10(), alr.HoaDonThang10()})); 
+        chart.addData(new ModelChart("Tháng 11", new double[]{alr.HopDongThang11(), alr.HoaDonThang11()}));
+        chart.addData(new ModelChart("Tháng 12", new double[]{alr.HopDongThang12(), alr.HoaDonThang12()}));
+        chart.start();
+    }//GEN-LAST:event_btnCaNamActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCaNam;
     private com.ViewGiaoDienChinh.component.Card card1;
-    private com.ViewGiaoDienChinh.component.Card card2;
     private com.ViewGiaoDienChinh.component.Card card3;
-    private com.ViewGiaoDienChinh.component.Card card4;
     private com.ViewGiaoDienChinh.component.Card card5;
+    private javax.swing.JComboBox<String> cbbQuy;
     private com.raven.chart.CurveChart chart;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLayeredPane panel;
     private com.ViewGiaoDienChinh.swing.PanelBorder panelBorder1;
-    private com.ViewGiaoDienChinh.swing.PanelBorder panelBorder2;
-    private javax.swing.JTable tblTop10;
+    private com.raven.javaswingdev.chart.PieChart pieChart1;
     // End of variables declaration//GEN-END:variables
 }
