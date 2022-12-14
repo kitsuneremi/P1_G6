96a69794bf2e7e57916de549212064e342caaf25
ShiXinNe <118367807+ShiXinNe@users.noreply.github.com>
2022-12-15 00:21:03 +0700

    sửa

--- ui-dashboard-001/src/com/raven/form/Form_hopdong.java
+++ ui-dashboard-001/src/com/raven/form/Form_hopdong.java
@@ -1219,7 +1219,7 @@ boolean sendemail() {
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject("Quý khách đã tạo hợp đồng thành công");
-            message.setText("Thank you for belive our service~ Hợp đồng của quý khách sẽ hết hạn vào ngày: " + txt_ngayhethan);
+            message.setText("Thank you for belive our service~ Hợp đồng của quý khách sẽ hết hạn vào ngày: " + txt_ngayhethan.getText());
             Transport.send(message, "lilypeachew@gmail.com", "qgvvffdbtvsfpsav");
             return true;
         } catch (MessagingException mex) {
